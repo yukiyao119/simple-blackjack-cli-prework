2d6b1ca9b2e4813cea530ce32806481a2e5b5a04
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-07-29 03:05:33 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,9 +23,9 @@ def get_user_input
   input = gets
 end
 
-def end_game
+def end_game(card_total)
   # code #end_game here
-  puts "Sorry, you hit 27. Thanks for playing!"
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
