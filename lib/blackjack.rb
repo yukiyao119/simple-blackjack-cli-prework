5d04b6bc838a32754635f0bf1448673f211a191a
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-07-29 03:02:41 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,29 +1,39 @@
 def welcome
   # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
   # code #deal_card here
+  rand((1..11))
 end
 
-def display_card_total
+def display_card_total(card_total)
   # code #display_card_total here
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
   # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
   # code #get_user_input here
+  input = gets
 end
 
 def end_game
   # code #end_game here
+  puts "Sorry, you hit 27. Thanks for playing!"
 end
 
 def initial_round
   # code #initial_round here
+  sum = 2.times do 
+    deal_card
+  end 
+  display_card_total(sum)
 end
 
 def hit?
